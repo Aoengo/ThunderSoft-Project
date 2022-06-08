f5b0346a8796bee587b26ab89d825a60cd5429c9
Aoengo <aoengo@outlook.com>
2022-06-07 23:56:46 -0700

    fix a bug

--- core-device/thread_device/.modules.order.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/gabriel/Desktop/ThunderSoft-Project/core-device/thread_device/modules.order := {   echo /home/gabriel/Desktop/ThunderSoft-Project/core-device/thread_device/ThunderSoft_Project.ko; :; } | awk '!x[$$0]++' - > /home/gabriel/Desktop/ThunderSoft-Project/core-device/thread_device/modules.order
--- core-device/thread_device/src/core_semaphore.c
+++ core-device/thread_device/src/core_semaphore.c
@@ -3,5 +3,5 @@
 
 void thread_sema_init(){
     sema_init(&dev.timer_sema1,0);
-    sema_init(&dev.timer_sema1,0);
+    sema_init(&dev.timer_sema2,0);
 }
