df6e5c47cd925f3a839ab237d3a2ae5b4cb6467a
Aoengo <aoengo@outlook.com>
2022-06-13 11:22:25 -0700

    add funtion about log file

--- driver_app/.modules.order.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/gabriel/Desktop/ThunderSoft-Project/driver_app/modules.order := {   echo /home/gabriel/Desktop/ThunderSoft-Project/driver_app/ThunderSoft_Project.ko; :; } | awk '!x[$$0]++' - > /home/gabriel/Desktop/ThunderSoft-Project/driver_app/modules.order
